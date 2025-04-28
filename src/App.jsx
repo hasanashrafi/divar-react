8a186fa0ef9d9c8b12a14fb498ddc2c781ca9148
Hasan Ashrafi <yasashrafi@gmail.com>
2025-04-28 10:39:58 +0330

    create app

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+ 
+
+  return (
+    
+      <div className="min-h-screen">
+       <p className="p-2 bg-red-700">Divar app</p>
+      </div>
+      
+    
+  )
+}
+
+export default App
